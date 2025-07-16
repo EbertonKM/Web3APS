a50e0e972b1275e730e9d174f81acdde0cd3413b
EbertonKM <144098194+EbertonKM@users.noreply.github.com>
2025-07-16 16:11:19 -0300

    Update page.tsx
    Código redundante removido

--- aps-frontend/app/page.tsx
+++ aps-frontend/app/page.tsx
@@ -8,29 +8,6 @@ import { RastreamentoStatus } from "@/types/RastreamentoStatus";
 import React, { useEffect, useState } from "react"
 
 export default function Home() {
-    const [encomendas, setEncomendas] = useState<Encomenda[]>([])
-    const [entregadores, setEntregadores] = useState<Entregador[]>([])
-    const [rastreamentoStatus, RastreamentoStatus] = useState<RastreamentoStatus[]>([])
-
-    const carregarEncomendas = async () => {
-        const res = await getEncomendas()
-        setEncomendas(res.data)
-    }
-    const carregarEntregadores = async () => {
-        const res = await getEntregadores()
-        setEntregadores(res.data)
-    }
-    const carregarRastreamentos = async () => {
-        const res = await getRastreamentos()
-        RastreamentoStatus(res.data)
-    }
-
-    useEffect(() => {
-        carregarEncomendas()
-        carregarEntregadores()
-        carregarRastreamentos()
-    }, []);
-    
     return(
         <div className="main-text">
             Bem vindo ao site de entregas! <br />
